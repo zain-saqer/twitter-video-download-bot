c4ef914a318484954964f84b3eb5d0a12f267c3e
zain <zainsaqer@gmail.com>
2023-01-06 15:35:06 +0100

    use .properties instead of env var

--- src/main/java/de/saqer/twittervideodownloadbot/twitter/oauth2/awslambdahandler/AuthorizeRedirectionHandler.java
+++ src/main/java/de/saqer/twittervideodownloadbot/twitter/oauth2/awslambdahandler/AuthorizeRedirectionHandler.java
@@ -7,17 +7,26 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import de.saqer.twittervideodownloadbot.twitter.oauth2.http.Authorization;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class AuthorizeRedirectionHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
+    private static Properties properties;
 
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
 
         LambdaLogger logger = context.getLogger();
 
-        Authorization authorization = new Authorization(System.getenv("TWITTER_CLIENT_ID"), System.getenv("TWITTER_OAUTH2_REDIRECTION_URI"));
+        Authorization authorization;
+        try {
+            authorization = new Authorization(readPropertyFromPropertiesFile("twitter.clientId"), readPropertyFromPropertiesFile("twitter.oauth20_redirection_uri"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         logger.log("redirect to authorize url");
 
@@ -30,5 +39,18 @@ public class AuthorizeRedirectionHandler implements RequestHandler<APIGatewayV2H
         response.setHeaders(headers);
 
         return response;
+    }
+
+    private static String readPropertyFromPropertiesFile(String name) throws IOException {
+        String propertyFilename = "config.properties";
+        if (properties == null) {
+            properties = new Properties();
+            properties.load(AuthorizeRedirectionHandler.class.getClassLoader().getResourceAsStream(propertyFilename));
+        }
+        String property = properties.getProperty(name);
+        if (property == null) {
+            throw new RuntimeException(String.format("property %s not found in %s file", name, propertyFilename));
+        }
+        return property;
     }
 }
